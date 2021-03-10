23668573ec31df95ea381a518f6d38c8d2b8b099
bink <eng116dtu@gmail.com>
2021-03-11 03:00:04 +0700

    init

---
+++ TimerBN/Helper.swift
@@ -0,0 +1,14 @@
+//
+//  Helper.swift
+//  TimerBN
+//
+//  Created by Binh Nguyen on 11/03/2021.
+//
+
+import Foundation
+
+enum TimerMode {
+    case running
+    case paused
+    case initial
+}
