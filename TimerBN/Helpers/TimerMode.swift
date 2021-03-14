26cd5a811701934282d6cb587310dd030fca1871
bink <eng116dtu@gmail.com>
2021-03-14 19:21:16 +0700

    update

---
+++ TimerBN/Helpers/TimerMode.swift
@@ -0,0 +1,23 @@
+//
+//  Helper.swift
+//  TimerBN
+//
+//  Created by Binh Nguyen on 11/03/2021.
+//
+
+import Foundation
+
+enum TimerMode {
+    case running
+    case paused
+    case initial
+}
+
+func secondsToMinutesAndSeconds(seconds: Int) -> String {
+    let minutes = "\((seconds % 3600) / 60)"
+    let seconds = "\((seconds % 3600) % 60)"
+    let minuteStamp = minutes.count > 1 ? minutes : "0" + minutes
+    let secondStamp = seconds.count > 1 ? seconds : "0" + seconds
+    
+    return "\(minuteStamp) : \(secondStamp)"
+}
